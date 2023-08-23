489cdcfe6ff432428a09b4aca810bdf7e12d55e4
Illuminatus66 <varunsodhani123@gmail.com>
2023-08-23 13:41:17 +0530

    App with minimal UI

--- ticketing_app/urls.py
+++ ticketing_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.shortcuts import redirect
 from .views import admin_view, dashboard, auth
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('', lambda request: redirect('ticketing_app:login'), name='home'),
     path('ticketing_app/register/', auth.register, name='register'),
     path('ticketing_app/login/', auth.user_login, name='login'),
     path('ticketing_app/dashboard/', dashboard.user_dashboard, name='user_dashboard'),
--- ticketing_project/settings.py
+++ ticketing_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xp-)!0l(1je+($x5tw57_b1*e-!@ol#b#%wd&vsqv_*8va9)iq'
+SECRET_KEY = config('SECRET_KEY', default= 'django-insecure-xp-)!0l(1je+($x5tw57_b1*e-!@ol#b#%wd&vsqv_*8va9)iq')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ticket-illuminatus66.pythonanywhere.com', 'illuminatus66.pythonanywhere.com'] 
 
 
 # Application definition
@@ -123,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_HSTS_SECONDS = 31536000  # 1 year (enforce HTTPS)
+SECURE_SSL_REDIRECT = True  # Redirect all HTTP requests to HTTPS
+
+SESSION_COOKIE_SECURE = True  # Use secure-only session cookies
+CSRF_COOKIE_SECURE = True  # Use secure-only CSRF cookies
