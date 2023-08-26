90568a9466e823e1fcdf860e4afea77353b50f48
Illuminatus66 <varunsodhani123@gmail.com>
2023-08-27 01:15:47 +0530

    changes in timezone to reflect local conditions

--- ticketing_project/settings.py
+++ ticketing_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+import pytz
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -106,7 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
+
 
 USE_I18N = True
 
